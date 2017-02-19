1b201d6216ae26893ac393816e5560f78b61d68d
David Glines <dglines86@gmail.com>
2017-02-18 18:23:10 -0800

    fields added to Tile class

--- sliderpuzzle/src/model/Tile.java
+++ sliderpuzzle/src/model/Tile.java
@@ -1,8 +1,22 @@
 package model;
 
+/**
+ * Represents an individual tile on the Slider Puzzle.
+ * 
+ * @author David
+ * @author Alvin
+ *
+ */
 public class Tile {
 
-	public void davidsMethod(){
-		System.out.print("davids method");
-	}
+	/** The number on the tile. */
+	private int myNumber;
+	
+	/** The row of the Tile. */
+	private int myRow;
+	
+	/** The column of the Tile. */
+	private int myColumn;
+	
+	
 }
