ab3f4d50b8be2b26bbf3d5996948207ea37473f7
csHourglass <csHourglass@live.com>
2017-02-19 05:44:48 -0800

    yah

--- sliderpuzzle/src/view/SliderView.java
+++ sliderpuzzle/src/view/SliderView.java
@@ -4,19 +4,12 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-<<<<<<< HEAD
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
-=======
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintStream;
 import java.util.Scanner;
->>>>>>> refs/remotes/origin/master
-
 import javax.imageio.ImageIO;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
