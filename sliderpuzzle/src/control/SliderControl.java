9e3ddb706b09c2b0cb5a9751eb9931a4685f7225
vats96ashu@gmail.com <vats96ashu@gmail.com>
2017-02-18 19:09:42 -0800

    updating

---
+++ sliderpuzzle/src/control/SliderControl.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package control;
+
+import model.Slider;
+
+/**
+ * @author aashishkumar
+ *
+ */
+public class SliderControl {
+	
+	//private int [][] numbers = new int (Slider);
+	Slider slider = new Slider();
+	private boolean hasWon(){
+		//if(slider)
+		return false;
+		
+	}
+
+}
