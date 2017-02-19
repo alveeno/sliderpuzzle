2f356b4f7804d6d2f0e11375775d9d87222f5e28
Alex Reid <csHourglass@live.com>
2017-02-18 18:55:37 -0800

    Added setUpMenuBar()

--- sliderpuzzle/src/view/SliderView.java
+++ sliderpuzzle/src/view/SliderView.java
@@ -5,13 +5,11 @@ public class SliderView {
 	    System.out.println("duc");
 	    System.out.println("duc2");
 	}
-<<<<<<< HEAD
 	void duc2() {
 	    System.out.println("test");
-=======
+	}
 	void al()	{
 		
->>>>>>> branch 'master' of https://github.com/alveeno/sliderpuzzle.git
 	}
 	public int zero = 0;
 }
