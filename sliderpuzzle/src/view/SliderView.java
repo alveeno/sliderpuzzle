f7f61e0698d0960f59961e81bd12ff222e51059a
Duc P. Nguyen <david.nguyen.2302@gmail.com>
2017-02-18 18:57:35 -0800

    set up JFrame

--- sliderpuzzle/src/view/SliderView.java
+++ sliderpuzzle/src/view/SliderView.java
@@ -1,17 +1,39 @@
 package view;
 
-public class SliderView {
-	void duc() {
-	    System.out.println("duc");
-	    System.out.println("duc2");
-	}
-<<<<<<< HEAD
-	void duc2() {
-	    System.out.println("test");
-=======
-	void al()	{
-		
->>>>>>> branch 'master' of https://github.com/alveeno/sliderpuzzle.git
-	}
-	public int zero = 0;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SliderView extends JFrame {
+    
+    private static final Dimension DEFAULT_SIZE = new Dimension(400, 400);
+    
+    private Dimension myDimension;
+
+    private JPanel myGameBoard;
+
+    
+    public SliderView() {
+        super("Slider Game");
+        setUpMenuBar();
+    }
+    
+    public void start() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(true);
+        myDimension = DEFAULT_SIZE;
+        setGUILayout();
+        setMinimumSize(myDimension);
+        pack();
+        setVisible(true);
+    }
+    
+    public void setGUILayout() {
+        add(myGameBoard, new BorderLayout().CENTER);
+        myGameBoard.setLayout(new GridLayout(4, 4));
+        
+    }
 }
