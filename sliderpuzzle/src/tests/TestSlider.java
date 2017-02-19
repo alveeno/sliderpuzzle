521d72063f2e09e82771da75b8198a4dfb1a47fa
alvin <alvin@10.239.26.189>
2017-02-18 23:46:02 -0800

    junit test

---
+++ sliderpuzzle/src/tests/TestSlider.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSlider {
+
+	@Before
+	public static void setUpBefore() {
+	}
+
+	@Test
+	public void testSlider() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+}
