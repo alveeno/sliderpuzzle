ddc79004b23ce50079cf327cf70e17e192bd9290
Alex Reid <csHourglass@live.com>
2017-02-18 19:18:06 -0800

    Added SliderMain class.

--- sliderpuzzle/src/view/SliderMain.java
+++ sliderpuzzle/src/view/SliderMain.java
@@ -3,7 +3,7 @@ package view;
 public class SliderMain {
 	public static void main(String[] args)	{
 		
-		//Creates an instance of SliderView and calls its start() method.
+		//Creates an instance of SliderView and calls its start() method
 		SliderView sliderGUI = new SliderView();
 		sliderGUI.start();
 		
