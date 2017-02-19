83da3bc5552103d2c40ef7a819d938dbbb04c205
wildergarcia <wildergarcia@10.239.16.133>
2017-02-18 22:16:30 -0800

    some ugly code!

--- sliderpuzzle/src/control/SliderControl.java
+++ sliderpuzzle/src/control/SliderControl.java
@@ -14,35 +14,49 @@ import view.SliderView;
  */
 public class SliderControl {
 	
-	//private int [][] numbers = new int (Slider);
+	private int [][] numbers = new int[4][4];
 	Slider slider = new Slider();
-	//Tile tile = new Tile();
+	Tile tile = new Tile();
 	
 	private boolean hasWon() {
-		//jdehwif(slider)
+		//if(slider.getSlider().equals(slider)) {
+		  
+			
+		//}
 		return false;
 		
 	}
 	
 	
-//	
-//	private boolean isMove(Slider theButtons) {
+	
+	private boolean isMove(Slider theButtons) {
+		return false;
 //		int j = tile.getRow();
-//		int x = theButtons.getRow();
-//		int i = tile.getColumns();
-//		int y = theButtons.getColumns();
+//	    int x = (theButtons).getRow();
+//		int i = tile.getColumn();
+//		int y = (theButtons).getColumn();
 //		if((j == x && i+1 == y) || (j == x && i+1 == y) || (j == x && i+1 == y) || (j == x && i+1 == y) ) {
 //			return true;
 //			
 //		}
-//		
-//	}
-//	private void move() {
-//		if(isMove){
-//			
-//		}
+		
 	}
+//		private void move() {
+//			//if(isMove){
+//				
+//			}
+		
+			private boolean isSolve() {
+				
+				int n = 16;
+				for (int i = 0; i< n; i++)
+					for (int j = 0; j< n; j++)
+						if (numbers[i][j] != numbers[i][j]) {
+							return false;
+				
+			}
+
+				return true;
+			}
+}
 	
-
-
-
