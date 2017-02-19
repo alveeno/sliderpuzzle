e332dc3023e18d27852e5c50d6b7b184b2ed8153
wildergarcia <wildergarcia@10.239.16.133>
2017-02-19 00:51:57 -0800

    here is something

--- sliderpuzzle/src/control/SliderControl.java
+++ sliderpuzzle/src/control/SliderControl.java
@@ -14,9 +14,10 @@ import view.SliderView;
  */
 public class SliderControl {
 	
-	private int [][] numbers = new int[4][4];
+	private int [][] numbers = new int[4][];
 	Slider slider = new Slider();
 //	Tile tile = new Tile();
+	private int[] tiles;
 	
 	private boolean hasWon() {
 		//if(slider.getSlider().equals(slider)) {
@@ -46,17 +47,50 @@ public class SliderControl {
 //				
 //			}
 		
-			private boolean isSolve() {
-				
-				int n = 16;
-				for (int i = 0; i< n; i++)
-					for (int j = 0; j< n; j++)
-						if (numbers[i][j] != numbers[i][j]) {
-							return false;
-				
-			}
+	/**
+	  * This check if the puzzle is solved already so it should stop doing it..
+	  * @return the program..
+	  */
+		private boolean isSolve() {
+			boolean flag;
+			int n = 16;
+			for (int i = 0; i< n; i++)
+				for (int j = 0; j< n; j++)
+					if (numbers[i][j] != numbers[i][j]) {
+						flag = false;
+			
+		}
 
-				return true;
+			return true;
+		}
+		/**
+		 * This check if the program can be solved it..
+		 * @return a counter if the program is divisible by 2..
+		 */
+		private boolean isSolvable () {
+		   int counter = 0;
+	        int numberTile = 16;
+			for (int i = 0; i < numberTile; i++) {
+	            for (int j = 0; j < i; j++) {
+	                if (tiles[j] > tiles[i])
+	                    counter++;
+	            
+	        }
+	        
+	    }
+			return counter % 2 == 0;
+
+		}
+		/**
+		 * I am trying to check if the puzzle cant be solved so 
+		 * it should shuffle the game to make it possible to solve..
+		 * @param theshuffle is good.
+		 * @return a new order of program..
+		 */
+		private Slider isnotSolvable(final Slider theshuffle) {
+			if(!isSolvable()) {
+				
 			}
+			return theshuffle;
+		}
 }
-	
