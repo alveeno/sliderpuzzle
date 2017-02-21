09f495d194d7929a5cc1fd6ab3e77b64e1f2be27
alvin <alvin@d-10-16-22-207.dhcp4.washington.edu>
2017-02-21 10:13:52 -0800

    FINAL PROJECT - WE DONE BOYS

--- sliderpuzzle/src/view/SliderMain.java
+++ sliderpuzzle/src/view/SliderMain.java
@@ -14,22 +14,22 @@ public class SliderMain {
 	    
 		//Creates an instance of SliderView and calls its start() method.
 		SliderView sliderGUI = new SliderView();
-		try {
-		    //UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-            UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
-            //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-        } catch (final UnsupportedLookAndFeelException ex) {
-            ex.printStackTrace();
-        } catch (final IllegalAccessException ex) {
-            ex.printStackTrace();
-        } catch (final InstantiationException ex) {
-            ex.printStackTrace();
-        } catch (final ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        /* Turn off metal's use of bold fonts */
-        UIManager.put("swing.boldMetal", Boolean.FALSE);
+//		try {
+//		    //UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+//            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+//            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+//            //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+//        } catch (final UnsupportedLookAndFeelException ex) {
+//            ex.printStackTrace();
+//        } catch (final IllegalAccessException ex) {
+//            ex.printStackTrace();
+//        } catch (final InstantiationException ex) {
+//            ex.printStackTrace();
+//        } catch (final ClassNotFoundException ex) {
+//            ex.printStackTrace();
+//        }
+//        /* Turn off metal's use of bold fonts */
+//        UIManager.put("swing.boldMetal", Boolean.FALSE);
         
         //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
